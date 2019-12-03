7d41e9a5be8df11981b27e2b6ea8067fdafb5e45
Winnie Yoe <winnieyoe@10-18-148-40.dynapool.wireless.nyu.edu>
2019-12-03 12:09:01 -0500

    live version

---
+++ traceroute-project/public/sketch.js
@@ -0,0 +1,128 @@
+// Open and connect socket
+let socket = io();
+// ipStack API for city,
+let ipURL = "http://api.ipstack.com/";
+let ips = [];
+let ipKey = "?access_key=1585e1ed2134c1238365356bd337912d";
+let fields = "&fields=ip,city,region_code,country_code,latitude,longitude"
+// IPs' lat, long, city from ipStack API
+let ipsData = {};
+//turf.js to calculate bearing(https://turfjs.org/docs/#bearing)
+//First lat, long is the values from 370 Jay Street, Brooklyn
+let lats = [40.76];
+let longs = [-73.984];
+let distances = [];
+let bearings = [];
+
+let renamethislater = 0
+let fmtRenamethislater = renamethislater
+
+function myFunc(){
+  let site = document.getElementById("site").value;
+  console.log("Button is pressed!", site);
+  socket.emit("website", site);
+}
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+  // Listen for confirmation of connection
+  socket.on('connect', function() {
+    console.log("Connected");
+  });
+
+  // Receive message from server
+  socket.on('traceroute', function(data){
+    // console.log(data);
+    // console.log(Object.keys(data).length);
+    // for (let i = 0; i< Object.keys(data).length; i++){
+    //   // console.log(data[i].ip);
+    //   if (data[i].ip != "*"){
+    //     ips.push(data[i].ip);
+    //   }
+    // }
+    // // console.log(ips);
+    // let ipStack = ipURL + ips.join() + ipKey;
+    // loadJSON(ipStack, gotIPdata);
+
+  });
+
+//Create array after 'finishTracing' when the traceroute has been completed -> .on ('close') in server side, instead of when it was running, otherwise will get more items than needed in array
+  socket.on('finishTracing', function(data) {
+    for (let i = 0; i< Object.keys(data).length; i++){
+      if (data[i].ip != "*"){
+        ips.push(data[i].ip);
+      }
+    }
+    renamethislater = ips.length
+    fmtRenamethislater = renamethislater
+    //Pass all IPs into ipStack's API
+    for (let i = 0; i<renamethislater; i++){
+      let ipStack = ipURL + ips[i] + ipKey + fields;
+      loadJSON(ipStack, geolocate);
+    }
+    // let ipStack = ipURL + ips[10] + ipKey;
+    // loadJSON(ipStack, gotIPdata);
+  });
+}
+
+//Get the lat, long, city data with ipStack's API
+function geolocate(data){
+  // if (data.latitude != null){
+  //   lats.push(data.latitude);
+  // }
+  // if (data.longitude != null){
+  //   longs.push(data.longitude);
+  // }
+  console.log("data", data);
+
+  if (data.longitude == null || data.latitude == null) {
+      fmtRenamethislater -= 1
+  }
+
+  if (data.longitude != null && data.latitude != null){
+    // console.log(data)
+    lats.push(data.latitude);
+    longs.push(data.longitude);
+    // distance();
+
+    if (lats.length == fmtRenamethislater) {
+      distance();
+    }
+  }
+
+}
+
+function distance(){
+  let from;
+  let to;
+  let options = {units: 'miles'};
+
+  for (let i=0; i<lats.length; i++){
+    if (lats[i+1]) {
+      from = turf.point([lats[i], longs[i]]);
+      to = turf.point([lats[i+1], longs[i+1]]);
+      // console.log("from", from.geometry.coordinates);
+      // console.log("to", to.geometry.coordinates);
+      let distance = turf.distance(from, to, options);
+      distances.push(distance);
+    }
+  }
+  console.log("distances", distances);
+}
+
+function bearing(){
+  // for (let i=0; i<lats.length; i++){
+  //   from = turf.point([lats[i], longs[i]]);
+  //   to = turf.point([lats[i+1], longs[i+1]]);
+  //   let bearing = turf.bearing(point1, point2);
+  //   bearings.push(bearing);
+  // }
+  //   console.log("bearings", bearings);
+}
+
+
+// Callback to draw position when new position is received
+// function gotHops(data){
+//   console.log(data);
+// }
---
+++ traceroute-project/server.js
@@ -0,0 +1,101 @@
+// adapted from https://github.com/mimiyin/collective-play-s18/blob/master/00_helloworld/02_helloworld-sockets/server.js
+
+// Create server
+let port = process.env.PORT || 8000;
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app).listen(port, function () {
+  console.log('Server listening at port: ', port);
+});
+
+// Tell server where to look for files
+app.use(express.static('public'));
+
+// traceroute
+const Traceroute = require('nodejs-traceroute');
+//
+// try {
+//     const tracer = new Traceroute();
+//     let hops = [];
+//     tracer
+//         // .on('pid', (pid) => {
+//         //     // console.log(`pid: ${pid}`);
+//         // })
+//         // .on('destination', (destination) => {
+//         //     console.log(`destination: ${destination}`);
+//         // })
+//         .on('hop', (hop) => {
+//           hops.push(hop)
+//
+//             // console.log(`hop: ${JSON.stringify(hop)}`);
+//         })
+//         .on('close', (code) => {
+//             console.log(`close: code ${code}`);
+//             console.log(hops);
+//         });
+//
+//     tracer.trace('example.com');
+// } catch (ex) {
+//     console.log(ex);
+// }
+
+
+// Create socket connection
+let io = require('socket.io').listen(server);
+
+// Listen for individual clients to connect
+io.sockets.on('connection',
+	// Callback function on connection
+  // Comes back with a socket object
+	function (socket) {
+
+		console.log("We have a new client: " + socket.id);
+    let hops = [];
+    // Listen for data from this client
+		socket.on('website', function(url) {
+      // Data can be numbers, strings, objects
+			console.log("Received: 'data' " + url);
+
+      try {
+          const tracer = new Traceroute();
+          tracer
+              .on('pid', (pid) => {
+                  // console.log(`pid: ${pid}`);
+              })
+              .on('destination', (destination) => {
+                  console.log(`destination: ${destination}`);
+              })
+              .on('hop', (hop) => {
+                hops.push(hop);
+                // if the last 5 hops are *, socket.emit 'badTrace'
+                // console.log(`hop: ${JSON.stringify(hop)}`);
+                //console.log(hops);
+                // socket.emit('traceroute', hops);
+              })
+              .on('close', (code) => {
+                  console.log(`close: code ${code}`);
+                  socket.emit('finishTracing', hops);
+              });
+
+          tracer.trace(url);
+          // console.log(hops);
+      } catch (ex) {
+          console.log(ex);
+      }
+
+			// Send it to all clients, including this one
+			// io.sockets.emit('data', data);
+
+      // Send it to all other clients, not including this one
+      //socket.broadcast.emit('data', data);
+
+      // Send it just this client
+
+		});
+
+    // Listen for this client to disconnect
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
